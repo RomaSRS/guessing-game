de50017a7a7591b6217b89b258a07bcbe8e4eb5b
romasrs <shaman.roman@tut.by>
2019-10-19 02:39:21 +0300

    feat: add function for Guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = null;
+      this.max = null;
+      this.result = null;    
+    }
 
     setRange(min, max) {
-
+        this.minVal = min;
+        this.maxVal = max; 
     }
 
     guess() {
-
+       return this.solution = Math.round((this.maxVal + this.minVal) / 2);
     }
 
     lower() {
-
+        this.maxVal = this.solution;
     }
 
     greater() {
-
+        this.minVal = this.solution;
     }
 }
 
